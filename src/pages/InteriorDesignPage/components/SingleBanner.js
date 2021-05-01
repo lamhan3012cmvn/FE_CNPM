03a279fce1372e2317a7be6a2e78fa1318b50654
lamancmvn <lamhoangan.work@gmail.com>
2021-05-01 18:29:53 +0700

    [new] slider banner interior design

--- src/pages/InteriorDesignPage/components/SingleBanner.js
+++ src/pages/InteriorDesignPage/components/SingleBanner.js
@@ -1,7 +1,14 @@
 import React from "react"
-import { Link } from "react-router-dom"
-import img1 from "../../../img/banner_img.png"
-const SingleBanner = () => {
+const SingleBanner = props => {
+  const { bannerImg } = props
+  console.log(
+    `LHA:  ===> file: SingleBanner.js ===> line 4 ===> bannerImg`,
+    bannerImg
+  )
+  console.log(
+    `LHA:  ===> file: SingleBanner.js ===> line 4 ===> bannerImg`,
+    bannerImg
+  )
   return (
     <div className="single_banner_slider">
       <div
@@ -11,7 +18,7 @@ const SingleBanner = () => {
         }}
       >
         <div className="banner_img d-none d-lg-block">
-          <img src={img1} alt="" />
+          <img src={bannerImg} alt="" />
         </div>
       </div>
     </div>
--- src/pages/InteriorDesignPage/index.js
+++ src/pages/InteriorDesignPage/index.js
@@ -1,10 +1,24 @@
+import { useState } from "react"
+import BannerPath from "../../common/components/BannerPath"
 import Breadcrumb from "../../common/components/Breadcrumb"
 import SingleBanner from "./components/SingleBanner"
 const InteriorDesignPage = () => {
+  const img2 =
+    "https://firebasestorage.googleapis.com/v0/b/cnpm-e5af3.appspot.com/o/InteriorDesign%2FBannerInteriorDesign%2Fslide-thiet-ke-noi-that-2.jpg?alt=media&token=064831d6-b63c-4ac0-8a8e-585be58485ee"
+  const img1 =
+    "https://firebasestorage.googleapis.com/v0/b/cnpm-e5af3.appspot.com/o/InteriorDesign%2FBannerInteriorDesign%2Fslide-thiet-ke-noi-that-2.jpg?alt=media&token=064831d6-b63c-4ac0-8a8e-585be58485ee"
+  const [componentBanner, setComponentBanner] = useState([
+    {
+      component: <SingleBanner key={1} bannerImg={img1} />
+    },
+    {
+      component: <SingleBanner key={2} bannerImg={img2} />
+    }
+  ])
   return (
     <>
-      <Breadcrumb />
-      <SingleBanner />
+      {/* <Breadcrumb /> */}
+      <BannerPath propsComponents={componentBanner} />
     </>
   )
 }
